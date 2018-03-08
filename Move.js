b16adc9920b6476915e0565e45b09e6001f9f91f
recep sivri <rsivri@gtu.edu.tr>
2018-03-08 12:34:45 +0300

    rook complated

--- Move.js
+++ Move.js
@@ -1,4 +1,4 @@
-function move(stack,table,clickedButton,y, x) {
+function move(stackflag,stack,table,clickedButton,y, x) {
 
     for(var i=0;i<8;++i)
     {
@@ -11,7 +11,7 @@ function move(stack,table,clickedButton,y, x) {
         }
 
     }
-
+    var flag=0;
     if(stack.length===1)
     {
         var clicked2=clickedButton.innerHTML.toString().split("//")[1];
@@ -32,102 +32,27 @@ function move(stack,table,clickedButton,y, x) {
     {
         var piece=clicked.split(".")[0];
 
-        if(piece==="wpawn")
-        {
-            var point = {X:x, Y:y};
-            stack.push(point);
-            var orthogonalRight=table[y-1][x+1].innerHTML.toString().split("//")[1];
-            var orthogonalLeft=table[y-1][x-1].innerHTML.toString().split("//")[1];
-            var up=table[y-1][x].innerHTML.toString().split("//")[1];
-            if(orthogonalRight!==undefined&&orthogonalRight.split(".")[0].charAt(0)!=='w'&&orthogonalRight.split(".")[0].charAt(0)!=='e')
-            {
-                table[y-1][x+1].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-            }
-            if(orthogonalLeft!==undefined&&orthogonalLeft.split(".")[0].charAt(0)!=='w'&&orthogonalLeft.split(".")[0].charAt(0)!=='e')
-            {
-                table[y-1][x-1].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-            }
+        if(piece==="wpawn") {
+            flag=1;
 
-            if(y===6)
-            {
-                table[y][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-                table[y-1][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-                table[y-2][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-            }
-            else
-            {
-                if(up!==undefined&&up.split(".")[0].charAt(0)!=='w'&&up.split(".")[0].charAt(0)!=='e')
-                {
-                    //table[y-1][x].style="height:75px;width:75px;";
-                }
-                else
-                if(y-1>=0)
-                {
-                    table[y][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-                    table[y-1][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
-                }
-            }
-
-
+            stackflag.push(flag);
+            pawnMarkArea(x,y,stack,table)
+        }
+        if(piece==="wrook") {
+            flag=2;
+            stackflag.push(flag);
+            rookMarkArea(x,y,stack,table)
         }
 
     }
 
     if(stack.length===2)
     {
-        var currCoor=stack.pop();
-        var oldCoor=stack.pop();
-        var clickedMove=table[oldCoor.Y][oldCoor.X].innerHTML.toString().split("//")[1];
-        if(clickedMove!==undefined)
-        {
-            var pieceMove = clickedMove.split(".")[0];
-            if(pieceMove==="wpawn")
-            {
-                if(oldCoor.Y==6&&currCoor.Y==4&&oldCoor.X==currCoor.X)
-                {
-                    table[oldCoor.Y][oldCoor.X].style="height:75px;width:75px;";
-                    table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
-                    table[currCoor.Y][currCoor.X].innerHTML = '<img src="img//wpawn.png" style="height:35px;width:35px"/>';
-                }
-                else
-                {
-                    if(oldCoor.Y-currCoor.Y==1&&oldCoor.X-currCoor.X==1)
-                    {
-                        var orthogonalLeft=table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1];
-                        if(orthogonalLeft!==undefined&&orthogonalLeft.split(".")[0].charAt(0)!=='w'&&orthogonalLeft.split(".")[0].charAt(0)!=='e')
-                        {
-                            table[currCoor.Y][currCoor.X].innerHTML = '<img src="img//wpawn.png" style="height:35px;width:35px"/>';
-                            table[oldCoor.Y][oldCoor.X].innerHTML = '<img src="img//empty.png" style="height:35px;width:35px"/>';
-                        }
-                    }
-                    if(oldCoor.Y-currCoor.Y==1&&oldCoor.X-currCoor.X==-1)
-                    {
-                        var orthogonalRight=table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1];
-                        if(orthogonalRight!==undefined&&orthogonalRight.split(".")[0].charAt(0)!=='w'&&orthogonalRight.split(".")[0].charAt(0)!=='e')
-                        {
-                            table[currCoor.Y][currCoor.X].innerHTML = '<img src="img//wpawn.png" style="height:35px;width:35px"/>';
-                            table[oldCoor.Y][oldCoor.X].innerHTML = '<img src="img//empty.png" style="height:35px;width:35px"/>';
-                        }
-                    }
-
-                    if(oldCoor.Y-currCoor.Y==1&&oldCoor.X==currCoor.X)
-                    {
-                        var up=table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1];
-                        if(!(up!==undefined&&up.split(".")[0].charAt(0)!=='w'&&up.split(".")[0].charAt(0)!=='e'))
-                        {
-                            table[oldCoor.Y][oldCoor.X].style="height:75px;width:75px;";
-                            table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
-                            table[currCoor.Y][currCoor.X].innerHTML = '<img src="img//wpawn.png" style="height:35px;width:35px"/>';
-                        }
-
-                    }
-                }
-
-            }
-
-        }
-
-
+        var a=stackflag.pop()
+        if(a===1)
+            pawnMove(stack,table)
+        if(a===2)
+            rookMove(stack,table)
     }
 
 
---
+++ rook.js
@@ -0,0 +1,113 @@
+function rookMarkArea(x,y,stack,table)
+{
+        var point = {X:x, Y:y};
+        stack.push(point);
+        table[y][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+
+        var k=y-1;
+        while(k>=0&&table[k][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+        {
+            table[k][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+            --k;
+        }
+        if(table[k][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='e'&&table[k][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+            table[k][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+
+        var l=x-1;
+        while(l>=0&&table[y][l].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+        {
+            table[y][l].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+            --l;
+        }
+        if(table[y][l].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='e'&&table[y][l].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+            table[y][l].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+
+        var j=x+1;
+        while(j<8&&table[y][j].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+        {
+            table[y][j].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+            ++j;
+        }
+        if(table[y][j].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='e'&&table[y][j].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+            table[y][j].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+
+        var i=y+1;
+        while(i<8&&table[i][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+        {
+            table[i][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+            ++i;
+        }
+        if(table[i][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='e'&&table[i][x].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+            table[i][x].style="height:75px;width:75px;border-color:blue;borderwidth:6px";
+
+}
+function rookMove(stack,table)
+{
+    var currCoor=stack.pop();
+    var oldCoor=stack.pop();
+
+    if(currCoor.X===oldCoor.X)
+    {
+          if(currCoor.Y<oldCoor.Y)
+          {
+                var i=currCoor.Y+1;
+                while(i<oldCoor.Y&&table[i][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+                    ++i;
+                if(i===oldCoor.Y)
+                {
+                    if(oldCoor.Y&&table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+                    {
+                        table[currCoor.Y][currCoor.X].innerHTML='<img src="img//wrook.png" style="height:35px;width:35px"/>';
+                        table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
+                    }
+                }
+          }
+        if(currCoor.Y>oldCoor.Y)
+        {
+            var i=oldCoor.Y+1;
+            while(i<currCoor.Y&&table[i][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+                ++i;
+            if(i===currCoor.Y)
+            {
+                if(currCoor.Y&&table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+                {
+                    table[currCoor.Y][currCoor.X].innerHTML='<img src="img//wrook.png" style="height:35px;width:35px"/>';
+                    table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
+                }
+            }
+        }
+    }
+
+    if(currCoor.Y===oldCoor.Y)
+    {
+        if(currCoor.X<oldCoor.X)
+        {
+            var i=currCoor.X+1;
+            while(i<oldCoor.X&&table[currCoor.Y][i].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+                ++i;
+            if(i===oldCoor.X)
+            {
+                if(oldCoor.X&&table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+                {
+                    table[currCoor.Y][currCoor.X].innerHTML='<img src="img//wrook.png" style="height:35px;width:35px"/>';
+                    table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
+                }
+            }
+        }
+        if(currCoor.X>oldCoor.X)
+        {
+            var i=oldCoor.X+1;
+            while(i<currCoor.X&&table[currCoor.Y][i].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)==='e')
+                ++i;
+            if(i===currCoor.X)
+            {
+                if(currCoor.X&&table[currCoor.Y][currCoor.X].innerHTML.toString().split("//")[1].split(".")[0].charAt(0)!=='w')
+                {
+                    table[currCoor.Y][currCoor.X].innerHTML='<img src="img//wrook.png" style="height:35px;width:35px"/>';
+                    table[oldCoor.Y][oldCoor.X].innerHTML='<img src="img//empty.png" style="height:35px;width:35px"/>';
+                }
+            }
+        }
+    }
+
+}
