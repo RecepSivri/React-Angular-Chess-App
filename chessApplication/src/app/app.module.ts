b26089a38da2dd948ae6500c7270a8407c0f0c2c
recep sivri <recep.sivri.eskipazar@gmail.com>
2019-01-02 16:15:51 +0300

    jquery scripts added

--- chessApplication/src/app/app.module.ts
+++ chessApplication/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/language/', '.json');
