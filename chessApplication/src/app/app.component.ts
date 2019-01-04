6d70d3d01edcc71e3db1b01a175c87ffd5fb82e0
recep sivri <recep.sivri.eskipazar@gmail.com>
2019-01-05 01:10:54 +0300

    repair jquery

--- chessApplication/src/app/app.component.ts
+++ chessApplication/src/app/app.component.ts
@@ -1,41 +1,11 @@
-import { Component, OnInit  } from '@angular/core';
-
-import {TranslateService} from "@ngx-translate/core";
-import * as $ from 'jquery';
-
-declare var jQuery:any;
-declare var $:any;
+import { Component } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'chessApplication';
-
-  public ngOnInit()
-  {
-    $(document).ready(function(){
-      $("button").click(function(){
-        var div = $("div");
-        div.animate({left: '100px'}, "slow");
-        div.animate({fontSize: '5em'}, "slow");
-      });
-    });
-  }
+export class AppComponent {
 
 
-  constructor(translate: TranslateService) {
-
-
-
-
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('tr');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('tr');
-
-
-  }
 }
--- chessApplication/src/app/app.module.ts
+++ chessApplication/src/app/app.module.ts
@@ -1,37 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
-import {HttpClientModule, HttpClient} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/language/', '.json');
-}
-
+import { HeaderComponentComponent } from './header-component/header-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
