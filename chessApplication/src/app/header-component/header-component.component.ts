18b3006f3456edbec2c0edad9e1b37e4630cbc0c
recep sivri <recep.sivri.eskipazar@gmail.com>
2019-01-07 00:08:23 +0300

    language swithcer fixed

--- chessApplication/src/app/header-component/header-component.component.ts
+++ chessApplication/src/app/header-component/header-component.component.ts
@@ -1,21 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
+import {log} from "util";
+import { TranslateService } from '@ngx-translate/core';
 declare var $: any;
 @Component({
   selector: 'app-header-component',
   templateUrl: './header-component.component.html',
   styleUrls: ['./header-component.component.css']
 })
-export class HeaderComponentComponent implements OnInit {
+export class HeaderComponentComponent implements OnInit ,OnChanges {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
+
 
   public ngOnInit()
   {
+    this.switchLanguage('tr')
+    var a=function(){
+      this.switchLanguage('tr')
+
+    }
+    var b=function(){
+      this.switchLanguage('en')
+    }
+
     $(window).on('load', function() {
       $("#country_selector").countrySelect({
 
-      });
+      })
+
+
+
     });
 
+    $("#country_selector").change(function() {
+
+      if(this.value=='Tr')
+        a();
+      if(this.value=='Us')
+        b()
+
+
+    });
+
+
+  }
+  public ngOnChanges(){
+    console.log("asdasdsad")
   }
 }
