dafed7d896a84e6ab7def75d0a273ced5f3ec32a
recep sivri <recep.sivri.eskipazar@gmail.com>
2019-01-08 16:56:08 +0300

    pieces layed out

--- chessApplication/src/app/table-component/table-component.component.ts
+++ chessApplication/src/app/table-component/table-component.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TableComponentComponent implements OnInit {
 
   table = [
-    [{id:0,piece:"wrook"},{id:1,piece:"wknight"},{id:2,piece:"wbishop"},{id:3,piece:"wqueen"},
-      {id:4,piece:"wking"},{id:5,piece:"wbishop"},{id:6,piece:"wknight"},{id:7,piece:"wrook"}],
+    [{id:0,piece:"wrook"},{id:1,piece:"whorse"},{id:2,piece:"wbishop"},{id:3,piece:"wqueen"},
+      {id:4,piece:"wking"},{id:5,piece:"wbishop"},{id:6,piece:"whorse"},{id:7,piece:"wrook"}],
 
     [{id:0,piece:"wpawn"},{id:1,piece:"wpawn"},{id:2,piece:"wpawn"},{id:3,piece:"wpawn"},
       {id:4,piece:"wpawn"},{id:5,piece:"wpawn"},{id:6,piece:"wpawn"},{id:7,piece:"wpawn"}],
@@ -29,8 +29,8 @@ export class TableComponentComponent implements OnInit {
     [{id:0,piece:"pawn"},{id:1,piece:"pawn"},{id:2,piece:"pawn"},{id:3,piece:"pawn"},
       {id:4,piece:"pawn"},{id:5,piece:"pawn"},{id:6,piece:"pawn"},{id:7,piece:"pawn"}],
 
-    [{id:0,piece:"rook"},{id:1,piece:"knight"},{id:2,piece:"bishop"},{id:3,piece:"wueen"},
-      {id:4,piece:"king"},{id:5,piece:"bishop"},{id:6,piece:"knight"},{id:7,piece:"rook"}]
+    [{id:0,piece:"rook"},{id:1,piece:"horse"},{id:2,piece:"bishop"},{id:3,piece:"queen"},
+      {id:4,piece:"king"},{id:5,piece:"bishop"},{id:6,piece:"horse"},{id:7,piece:"rook"}]
 
   ];
 
